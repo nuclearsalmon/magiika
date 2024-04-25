f1be08ab522be3619a9ceb8912255b4f0cef654a
Gustav Dersjö <gustav.dersjo@gmail.com>
2024-04-25 22:37:44 +0200

    Nilable @errors in MatchResult

--- src/magiika/util/match_result.cr
+++ src/magiika/util/match_result.cr
@@ -1,31 +1,43 @@
 module Magiika
   class MatchResult
-    property errors : Array(String)
-
     def initialize(
         @matched : Bool,
-        @errors : Array(String) = [] of String)
+        @errors : Array(String)? = nil)
+    end
+
+    private def errors
+      errors = @errors
+      if errors.nil?
+        errors = Array(String).new
+        @errors = errors
+      end
+      errors
     end
 
     def add_error(error : String)
-      @errors << error
+      errors << error
     end
 
     def merge!(other : MatchResult)
-      @errors.concat(other.errors)
+      errors.concat(other.errors)
     end
 
     def matched?
-      @matched && @errors.empty?
+      @matched && errors.empty?
     end
 
-    def errors?
-      @errors.empty?
+    def errors? : Array(String)?
+      @errors
+    end
+
+    def has_errors? : ::Boool
+      errors = @errors
+      !errors.nil? || errors.empty?
     end
 
     def defer_raise : Error::InternalMatchFail?
       return if matched?
-      @errors << "Failed to match" if @errors.empty?
+      errors << "Failed to match" if !has_errors?
       return Error::InternalMatchFail.new(@errors)
     end
 
