0c6d4f83c7d65e47c923a31b28aebd5c26ece5ca
gustavdersjo <gustav.dersjo@gmail.com>
2024-04-28 11:34:51 +0200

    added union to type alias

--- src/magiika/node/node.cr
+++ src/magiika/node/node.cr
@@ -3,7 +3,7 @@ require "../typing/typing.cr"
 
 module Magiika
   # Node as a type representation
-  alias NodeType = NodeClassBase.class | NodeStructBase.class # | Node::Class
+  alias NodeType = NodeClassBase.class | NodeStructBase.class | Node::Union # | Node::Class
   # Node as an instance
   alias NodeObj = NodeClassBase | NodeStructBase
   # Node as either a type representation or an instance
