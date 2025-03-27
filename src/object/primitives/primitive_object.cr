06f346992c01341e48c59a08566a9513566de21b
Gustav Dersjö <gustav.dersjo@gmail.com>
2025-03-27 16:04:31 +0100

    Fix primitive objects being mistakenly evaluated to Nil when they should return self as they are already considered evaluated, being objects.

--- src/object/primitives/primitive_object.cr
+++ src/object/primitives/primitive_object.cr
@@ -1,3 +1,10 @@
 abstract class Magiika::PrimitiveObject < Magiika::Object
   include Ast
+
+  # NOTE: A shim to ensure that even though this is an object and 
+  # thus doesn't require evaluation, it can still be used 
+  # in expressions the way one would expect.
+  def eval(scope : Scope) : Object
+    self
+  end
 end
